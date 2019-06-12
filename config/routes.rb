78b0e2fe68434decdd205a92d2c9c569d4cf6b0a
danielshow <danielshotonwa53@gmail.com>
2019-06-12 13:03:45 +0100

    add toggle to update blog status

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   get 'portfolio/:id' => 'portfolios#show', as: 'portfolio_show'
   get '/about' => 'pages#about'
   get '/contact' => 'pages#contact'
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
 end
