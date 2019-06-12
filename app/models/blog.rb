40af6b647b05265442d2eeab9e5982ea42e8ec5c
danielshow <danielshotonwa53@gmail.com>
2019-06-12 15:40:31 +0100

    implement default for portfolio model

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -4,4 +4,7 @@ class Blog < ApplicationRecord
   friendly_id :title, use: :slugged
   validates :title, presence: true
   validates :body, presence: true
+  validates_presence_of :topic_id
+
+  belongs_to :topic
 end
