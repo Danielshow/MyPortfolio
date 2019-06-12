974a54cf668b15ea30c3eaf2db934b806600a62e
danielshow <danielshotonwa53@gmail.com>
2019-06-12 02:25:25 +0100

    implemented crud functonality for portfolio feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio, except: [:new, :create, :index]
   def index
     @portfolio_items = Portfolio.all
   end
@@ -6,6 +7,8 @@ class PortfoliosController < ApplicationController
   def new
     @portfolio_item = Portfolio.new
   end
+
+  def show; end
 
   def create
     @portfolio_item = Portfolio.new(portfolio_params)
@@ -18,7 +21,29 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @portfolio_item.update(portfolio_params)
+      redirect_to portfolios_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_path, notice: 'Portfolio was deleted successfully.' }
+    end
+  end
+
   private
+
+  def set_portfolio
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
   def portfolio_params
     params.require(:portfolio).permit(:title,
                                       :subtitle,
