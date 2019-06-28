6a10e065f3452723c54d0e829dd4bffcde9d0ae6
danielshow <danielshotonwa53@gmail.com>
2019-06-28 22:49:29 +0100

    add associations and forms for comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # Comment resource
   resources :comments
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   put 'portfolios/sort' => 'portfolios#sort_portfolio'
